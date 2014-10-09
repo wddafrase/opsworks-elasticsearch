e49d177916ba1f546552c25f67c9a133635eceae
Aaron Frase <afrase@wddonline.com>
2014-10-09 10:12:43 -0400

    more changes

--- layer-custom/recipes/allocation-awareness.rb
+++
@@ -1,1 +0,0 @@
-node.default[:elasticsearch][:custom_config] =  {'node.rack_id' => "#{node[:opsworks][:instance][:availability_zone]}"}
--- layer-custom/recipes/esmonit.rb
+++
@@ -1,6 +0,0 @@
-template "/etc/monit.d/elasticsearch-monit.conf" do
-  source "elasticsearch.monitrc.conf.erb"
-  mode 0440
-  owner "root"
-  group "root"
-end
