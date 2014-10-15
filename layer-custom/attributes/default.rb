66f9e8b57356daf9ab71b7df1fc4afb22dfbd083
Aaron Frase <afrase@wddonline.com>
2014-10-15 15:56:24 -0400

    fix to defaults

--- layer-custom/attributes/default.rb
+++ layer-custom/attributes/default.rb
@@ -1,3 +1,8 @@
 default[:layer_custom][:key] = ''
 default[:layer_custom][:crt] = ''
-default[:layer_custom][:es_host] = node[:opsworks][:stack]['elb-load-balancers'][0][:dns_name]
+
+if node[:opsworks][:stack]['elb-load-balancers'].size > 0
+  default[:layer_custom][:es_host] = node[:opsworks][:stack]['elb-load-balancers'][0][:dns_name]
+else
+  default[:layer_custom][:es_host] = nil
+end
